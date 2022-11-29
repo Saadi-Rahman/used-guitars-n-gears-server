1027add2c5bbfac941995b10bfbfa2c059eb7247
Saadi-Rahman <sadi6280@gmail.com>
2022-11-29 06:17:27 +0600

    added get API for products and specific category

--- index.js
+++ index.js
@@ -6,13 +6,30 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const products = require('./data/products.json');
+
 
 app.get('/', (req, res) => {
     res.send('Used Guitars N Gears API is Running...');
 });
 
+
 app.get('/product-categories', (req, res) => {
     res.send(categories);
+});
+
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    const category_products = products.filter( p => p.category_id === id);
+    res.send(category_products);
+})
+
+
+app.get('/products/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedProduct = products.find( p => p._id === id);
+    res.send(selectedProduct);
 })
 
 
