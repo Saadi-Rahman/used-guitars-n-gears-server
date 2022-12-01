4978f156f9fdd550fbb97bf47f82342b203e958e
Saadi-Rahman <sadi6280@gmail.com>
2022-12-01 23:31:52 +0600

    added get API for jwt

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
+
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -18,10 +20,13 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run(){
     try{
+        // MongoDB collections
         const categoriesCollection = client.db('usedGuitarsNGears').collection('categories');
         const productsCollection = client.db('usedGuitarsNGears').collection('products');
         const bookingsCollection = client.db('usedGuitarsNGears').collection('bookings');
+        const usersCollection = client.db('usedGuitarsNGears').collection('users');
 
+        // get API to show all categories on the Home page
         app.get('/categories', async(req, res) => {
             const query = {};
             const cursor = categoriesCollection.find(query);
@@ -29,6 +34,7 @@ async function run(){
             res.send(categories);
         });
 
+        // get API to show a specific category on the Categories page
         app.get('/categories/:name', async(req, res) =>{
             const name = req.params.name;
             const query = { category_name: name };
@@ -36,6 +42,7 @@ async function run(){
             res.send(products);
         });
 
+        // get API to show all products on the Categories page
         app.get('/products', async(req, res) => {
             const query = {};
             const cursor = productsCollection.find(query);
@@ -43,6 +50,7 @@ async function run(){
             res.send(products);
         });
 
+        // get API to show a specific product and its' info on the productDetails page
         app.get('/products/:id', async(req, res) =>{
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
@@ -50,6 +58,7 @@ async function run(){
             res.send(products);
         });
 
+        // get API to show bookings data on the myOrders page
         app.get('/bookings', async(req, res) => {
             const email = req.query.email;
             const query = {email: email};
@@ -57,11 +66,32 @@ async function run(){
             res.send(bookings);
         });
 
+        // post API to send bookings data to MongoDB
         app.post('/bookings', async(req, res) =>{
             const booking = req.body
             const result = bookingsCollection.insertOne(booking);
             res.send(result);
         });
+
+        // get API for jwt
+        app.get('/jwt', async(req, res) =>{
+            const email = req.query.email;
+            const query = {email: email};
+            const user = await usersCollection.findOne(query);
+            if(user){
+                const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '1h'})
+                return res.send({accessToken: token});
+            }
+            res.status(403).send({accessToken: ''});
+        })
+
+        // post API to send users data to MongoDB
+        app.post('/users', async(req, res) =>{
+            const user = req.body;
+            console.log(user);
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
 
     }
     finally{
