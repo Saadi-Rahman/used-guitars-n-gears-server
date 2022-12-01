2ae5b8b2dfcba42ca40507a859be9a29bfa1d424
Saadi-Rahman <sadi6280@gmail.com>
2022-12-01 21:36:05 +0600

    added get API to show bookings data on client side

--- index.js
+++ index.js
@@ -50,13 +50,18 @@ async function run(){
             res.send(products);
         });
 
+        app.get('/bookings', async(req, res) => {
+            const email = req.query.email;
+            const query = {email: email};
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        });
 
         app.post('/bookings', async(req, res) =>{
-            const booking = req.body 
-            console.log(booking);
+            const booking = req.body
             const result = bookingsCollection.insertOne(booking);
             res.send(result);
-        })
+        });
 
     }
     finally{
