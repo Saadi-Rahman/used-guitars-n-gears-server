ae33d4c6726b336a88880b271fccca52996ea9bb
Saadi-Rahman <sadi6280@gmail.com>
2022-12-02 05:56:26 +0600

    create a custom middleware for jwt verify

--- index.js
+++ index.js
@@ -17,6 +17,25 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rf7qboo.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// custom middleware for verifyJWT
+function verifyJWT(req, res, next){
+
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send('unauthorized access');
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function(err, decoded){
+        if(err){
+            return res.status(403).send({message: 'forbidden access'})
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 
 async function run(){
     try{
@@ -59,8 +78,14 @@ async function run(){
         });
 
         // get API to show bookings data on the myOrders page
-        app.get('/bookings', async(req, res) => {
+        app.get('/bookings', verifyJWT, async(req, res) => {
             const email = req.query.email;
+            const decodedEmail = req.decoded.email;
+
+            if(email !== decodedEmail){
+                return res.status(403).send({message: 'forbidden access'});
+            }
+
             const query = {email: email};
             const bookings = await bookingsCollection.find(query).toArray();
             res.send(bookings);
